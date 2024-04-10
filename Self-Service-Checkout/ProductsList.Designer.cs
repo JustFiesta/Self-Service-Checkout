c8f98c48d87ecb0057aba597d17f61d57253b06f
Jakub <jakubowskijjal1@gmail.com>
2024-04-10 16:31:43 +0200

    add: new window to display products from a given category

---
+++ Self-Service-Checkout/ProductsList.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Self_Service_Checkout
+{
+    partial class ProductsList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductsList));
+            SuspendLayout();
+            // 
+            // ProductsList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(789, 744);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ProductsList";
+            Text = "Self-Service-Checkout";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
